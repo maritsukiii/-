1da9c5cf5d22d4969945da7a2efb249fcd9d31fd
maritsukiii <155792045+maritsukiii@users.noreply.github.com>
2024-10-26 21:53:53 +0300

    Update script3.js

--- script3.js
+++ script3.js
@@ -2,32 +2,28 @@ document.addEventListener('DOMContentLoaded', function () {
     const audioPlayer = document.getElementById('audioPlayer');
     const lyricsContainer = document.getElementById('lyricsContainer');
     const lyricsLines = lyricsContainer.getElementsByTagName('p');
-    let lastActiveIndex = -1;
-
+  
     function updateLyrics() {
-        const currentTime = audioPlayer.currentTime;
-
-        window.requestAnimationFrame(() => {
-            for (let i = 0; i < lyricsLines.length; i++) {
-                const lineTime = parseFloat(lyricsLines[i].getAttribute('data-time'));
-                const nextLineTime = lyricsLines[i + 1] ? parseFloat(lyricsLines[i + 1].getAttribute('data-time')) : Infinity;
-
-                if (currentTime >= lineTime && currentTime < nextLineTime) {
-                    if (lastActiveIndex !== i) {
-                        lyricsLines[i].classList.add('active');
-                        lyricsLines[i].scrollIntoView({ behavior: 'smooth', block: 'center' });
-                        lastActiveIndex = i;
-                    }
-                } else {
-                    lyricsLines[i].classList.remove('active');
-                }
-            }
-        });
+      const currentTime = audioPlayer.currentTime;
+  
+      for (let i = 0; i < lyricsLines.length; i++) {
+        const lineTime = parseFloat(lyricsLines[i].getAttribute('data-time'));
+        const nextLineTime = lyricsLines[i + 1] ? parseFloat(lyricsLines[i + 1].getAttribute('data-time')) : Infinity;
+  
+        if (currentTime >= lineTime && currentTime < nextLineTime) {
+          lyricsLines[i].classList.add('active');
+          lyricsLines[i].scrollIntoView({ behavior: 'smooth', block: 'center' });
+        } else {
+          lyricsLines[i].classList.remove('active');
+        }
+      }
     }
-
+  
     audioPlayer.addEventListener('timeupdate', updateLyrics);
+  });
 
-     const notification = document.getElementById('fullscreen-notification');
+   // Получаем элемент уведомления
+   const notification = document.getElementById('fullscreen-notification');
 
    // Добавляем обработчик события на нажатие
    notification.addEventListener('click', function() {
