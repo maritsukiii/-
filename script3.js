0eb26200e1db494d2dd2bba3e85bcbe73947a49d
maritsukiii <155792045+maritsukiii@users.noreply.github.com>
2024-10-26 21:48:16 +0300

    Update script3.js

--- script3.js
+++ script3.js
@@ -7,35 +7,31 @@ document.addEventListener('DOMContentLoaded', function () {
     function updateLyrics() {
         const currentTime = audioPlayer.currentTime;
 
-        for (let i = 0; i < lyricsLines.length; i++) {
-            const lineTime = parseFloat(lyricsLines[i].getAttribute('data-time'));
-            const nextLineTime = lyricsLines[i + 1] ? parseFloat(lyricsLines[i + 1].getAttribute('data-time')) : Infinity;
+        window.requestAnimationFrame(() => {
+            for (let i = 0; i < lyricsLines.length; i++) {
+                const lineTime = parseFloat(lyricsLines[i].getAttribute('data-time'));
+                const nextLineTime = lyricsLines[i + 1] ? parseFloat(lyricsLines[i + 1].getAttribute('data-time')) : Infinity;
 
-            if (currentTime >= lineTime && currentTime < nextLineTime) {
-                if (lastActiveIndex !== i) {
-                    lyricsLines[i].classList.add('active');
-                    lyricsLines[i].scrollIntoView({ behavior: 'smooth', block: 'center' });
-                    lastActiveIndex = i;
+                if (currentTime >= lineTime && currentTime < nextLineTime) {
+                    if (lastActiveIndex !== i) {
+                        lyricsLines[i].classList.add('active');
+                        lyricsLines[i].scrollIntoView({ behavior: 'smooth', block: 'center' });
+                        lastActiveIndex = i;
+                    }
+                } else {
+                    lyricsLines[i].classList.remove('active');
                 }
-            } else {
-                lyricsLines[i].classList.remove('active');
             }
-        }
+        });
     }
 
     audioPlayer.addEventListener('timeupdate', updateLyrics);
 
-    // Получаем элемент уведомления
     const notification = document.getElementById('fullscreen-notification');
-
-    // Добавляем обработчик события на нажатие
     if (notification) {
         notification.addEventListener('click', function() {
-            // Проверяем, есть ли класс "hidden"
             if (!notification.classList.contains('hidden')) {
-                // Добавляем класс, чтобы скрыть уведомление
                 notification.classList.add('hidden');
             }
         });
     }
-});
